d56a9f34e9d5f9e489d219871d4126a1b5d19b7d
Adit Goyal <42636217+Adit31@users.noreply.github.com>
2021-06-03 22:14:06 +0530

    Add files via upload

---
+++ Video Captioning/Data Preparation/Scripts/CreateVideoID.py
@@ -0,0 +1,32 @@
+import os
+import pandas as pd
+import pickle
+
+#Path of the folder containing csv files for captions
+folder_path = "UCFC-VD/Captions"
+
+files = os.listdir(folder_path)
+
+j = 0
+sentences = []
+sentences.append([])
+sentences.append([])
+for i in range(0, 13):
+    file_path = folder_path + "/" + files[i]
+    df = pd.read_csv(file_path)
+    for i in range(0, len(df['File Name'])):
+        sentences[0].append(df['Caption 1'][i])
+        sentences[0].append(df['Caption 2'][i])
+        sentences[0].append(df['Caption 3'][i])
+        sentences[0].append(df['Caption 4'][i])
+        sentences[0].append(df['Caption 5'][i])
+        
+        sentences[1].append(j)
+        sentences[1].append(j)
+        sentences[1].append(j)
+        sentences[1].append(j)
+        sentences[1].append(j)
+        j = j + 1
+        
+with open("ucfc-vd_sentence_videoID.pkl", 'wb') as fh:
+    pickle.dump(sentences, fh)
---
+++ Video Captioning/Data Preparation/Scripts/CreateVocabulary.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+import pickle
+
+#Folder location of all the Caption Files
+folder_path = "UCFC-VD/Captions"
+
+files = os.listdir(folder_path)
+word_list = []
+
+for i in range(0, 13):
+    file_path = folder_path + "/" + files[i]
+    df = pd.read_csv(file_path)
+    for i in range(0, len(df['File Name'])):
+        sentence1 = df['Caption 1'][i]
+        words = sentence1.strip().split()
+        for word in words:
+            if word.lower() not in word_list:
+                word_list.append(word.lower())
+        sentence2 = df['Caption 2'][i]
+        words = sentence2.strip().split()
+        for word in words:
+            if word.lower() not in word_list:
+                word_list.append(word.lower())
+        sentence3 = df['Caption 3'][i]
+        words = sentence3.strip().split()
+        for word in words:
+            if word.lower() not in word_list:
+                word_list.append(word.lower())
+        sentence4 = df['Caption 4'][i]
+        words = sentence4.strip().split()
+        for word in words:
+            if word.lower() not in word_list:
+                word_list.append(word.lower())
+        sentence5 = df['Caption 5'][i]
+        words = sentence5.strip().split()
+        for word in words:
+            if word.lower() not in word_list:
+                word_list.append(word.lower())
+
+word_list.sort()
+
+with open("ucfc-vd_WordList.pkl", 'wb') as fh:
+    pickle.dump(word_list, fh)
