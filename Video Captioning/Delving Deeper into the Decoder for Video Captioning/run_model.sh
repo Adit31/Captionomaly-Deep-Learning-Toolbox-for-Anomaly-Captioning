8d7e575784c772c650c30156bf5ca84970f4f7e4
Adit Goyal <42636217+Adit31@users.noreply.github.com>
2021-06-29 17:25:13 +0530

    Update run_model.sh

--- Video Captioning/Delving Deeper into the Decoder for Video Captioning/run_model.sh
+++ Video Captioning/Delving Deeper into the Decoder for Video Captioning/run_model.sh
@@ -1,4 +1,5 @@
 #Uncomment the test argument if you don't want to train the model (only for testing)
+#Set demo to True to test the model on individual samples
 TF_XLA_FLAGS=--tf_xla_cpu_global_jit \
 XLA_FLAGS=--xla_hlo_profile \
 CUDA_VISIBLE_DEVICES=1 \
@@ -7,5 +8,6 @@ python train_model.py \
     --ecores /Path-to-ResNeXt-Features \
     --tag    /Path-to-Tag_Feats-File \
     --ref    /Path-to-Ref-File \
+    --demo   False
 #    --test   /Path-to-Saved-Model \
     > test.log
