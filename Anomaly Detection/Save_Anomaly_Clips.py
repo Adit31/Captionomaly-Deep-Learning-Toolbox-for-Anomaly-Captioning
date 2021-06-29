0f21a18c92e973c11fa9d92e905c7847da9961c3
Adit Goyal <42636217+Adit31@users.noreply.github.com>
2021-06-29 23:09:56 +0530

    Update Save_Anomaly_Clips.py

--- Anomaly Detection/Save_Anomaly_Clips.py
+++ Anomaly Detection/Save_Anomaly_Clips.py
@@ -4,8 +4,11 @@ import os
 from os import listdir
 from os.path import isfile, join
 
+#Path of Directory containing the videos (.mp4) for anomaly detection
 test_videoClips_path = '/content/drive/MyDrive/AnomalyDetectionCVPR2018/SampleVideoClips'
+#Path of Directory containing anomaly score files (.txt) for each video used in anomaly detection
 eval_res_path = '/content/drive/MyDrive/Eval_Res'
+#Path of Directory to store the clipped videos
 Results_Path = "/content/drive/MyDrive/AnomalyDetectionCVPR2018/AnomalyClips/"
 
 if not os.path.exists(Results_Path):
@@ -17,23 +20,23 @@ All_test_videoClips = listdir(test_videoClips_path)
 All_eval_files.sort()
 All_test_videoClips.sort()
 
+#Threshold Value above which we consider the score as anomalous
+threshold = 0.5
+
 for i in range(len(All_eval_files)):
   eval_file_path = os.path.join(eval_res_path, All_eval_files[i])
   video_file_path = os.path.join(test_videoClips_path, All_test_videoClips[i])
   eval_file = open(eval_file_path, 'r')
   video_file = moviepy.editor.VideoFileClip(video_file_path)
-  print(video_file_path)
   aa=All_eval_files[i]
   aa=aa[0:-4]
   video_duration = int(video_file.duration)
-  print(video_duration)
   Each_segment_length = video_duration//32
 
   j = 1
   for each in eval_file:
-    print("test")
     Seg_score = float(each)
-    if Seg_score >= 0.5:
+    if Seg_score >= threshold:
       if j == 1:
         Anomaly_startTime = 0
       else:
